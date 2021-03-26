88736b16af344ffed385296fdd43cbcad4f1288a
Jonathan <void.knight09@gmail.com>
2021-03-26 14:03:43 +1300

    update combineReducer, createReducer types

--- src/combineReducers.d.ts
+++ src/combineReducers.d.ts
@@ -1,7 +1,6 @@
 import Rodux from "./index";
-type ReducerMap<S> = { [K in keyof S]: Rodux.Reducer<S[K], any> };
-declare function combineReducers<S>(reducers: ReducerMap<S>): Rodux.Reducer<S>;
+type ReducerMap<S, A extends Rodux.Action> = { [K in keyof S]: Rodux.Reducer<S[K], A> };
 declare function combineReducers<S, A extends Rodux.Action>(
-    reducers: ReducerMap<S>,
+    reducers: ReducerMap<S, A>,
 ): Rodux.Reducer<S, A>;
 export = combineReducers;
--- src/createReducer.d.ts
+++ src/createReducer.d.ts
@@ -1,14 +1,14 @@
-	/**
-	 * Creates a reducer that handles actions under `actionHandlers`.
-	 * Use the second overload for more explicit action handling
-	 * @param value
-	 * @param actionHandlers
-	 */
-	declare function createReducer<S, K extends keyof S>(
-		value: S[K],
-		actionHandlers: {
-			[name: string]: (state: S[K], action: AnyAction) => S[K] | S;
-		},
-    ): Reducer<S[K], AnyAction>;
-    
+import Rodux from "./index";
+
+/**
+ * Creates a reducer that handles actions under `actionHandlers`.
+ * Use the second overload for more explicit action handling
+ * @param value
+ * @param actionHandlers
+ */
+declare function createReducer<S, A extends Rodux.Action>(
+	value: S,
+	actionHandlers: Rodux.ActionHandlers<S, A>,
+): Rodux.Reducer<S, A>;
+
 export = createReducer;
